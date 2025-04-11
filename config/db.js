4283a5e714c410f9263cced0a74eb0ec63a018d1
Roberto <fizz.1214.cp.fizz@gmail.com>
2025-04-11 09:13:31 -0500

    Todo mal

---
+++ config/db.js
@@ -0,0 +1,18 @@
+// la carpeta db.js para conectar postgres 
+const { Pool } = require('pg');
+
+class Database {
+    constructor() {
+        this.pool = new Pool({
+            user: 'postgres',   //usuario de postgresql
+            host: 'localhost',      //localhost si mapeaste el puerto 5432 en docker
+            database: 'paciente', //nombre de la base de datos 
+            password: 'admin123', // cantraseña de la base de datos 
+            port: 5432, // puerto por defecto de postgresql
+        });
+  }
+    query(text, params) {
+        return this.pool.query(text, params);
+    }
+}
+module.exports= new Database();
---
+++ testConnection.js
@@ -0,0 +1,11 @@
+//la carpeta testConnection.js para ejecular la conexion si fue exitosa
+const db = require('./config/db');
+
+(async ()=>{
+    try {
+        const result = await db.query('SELECT * from usuario');
+        console.log('Conexion exitosa a la fecha y hora actual:',result.rows[0])
+    } catch (error) {
+        console.error('Error de conexion',error);
+    }
+})();
